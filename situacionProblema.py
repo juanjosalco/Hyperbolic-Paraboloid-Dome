fe8eb6032f333e66cea42d8db598190bc59ff839
juanjosalco <jjsc2003@live.com>
2022-03-31 18:51:02 -0600

    Formula para Volumen

--- situacionProblema.py
+++ situacionProblema.py
@@ -1,3 +1,34 @@
 """ Bienvenida Mary --- Ponte a Trabajar"""
 
-"""Hola"""
+
+
+
+
+
+
+
+
+
+
+
+from cmath import sqrt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+vVault = ((4*a*c)/b^3)*[(ly/4)*((ly^2/4)+(M*b^2/c))^(3/2)] + ((3*M*b^2*ly)/(8*c))*sqrt((ly^2/4)+(M*b^2/c)) + ((3*M^2*b^4)/(8*c^2)) * log(sqrt((ly^2/4)+(M*b^2)))
