fdb80f1bdb8ca21389d65a0cb2997c8bc295df5a
juanjosalco <jjsc2003@live.com>
2022-04-01 14:28:50 -0600

    Mejor presentación y eliminación de cosas innecesarias

--- situacionProblema.py
+++ situacionProblema.py
@@ -5,42 +5,54 @@ Terminado el 31 de Marzo de 2022
 """
 from cmath import log, sqrt
 def formulas(a,b,c,x,y,M):
-    print(a,b,c)
+    print(" El valor de a es: ", a, "\n", "El valor de b es: ", b, "\n", "El valor de c es: ", c)
     valor_1 = 0
     valor_2 = 0
     Hz = 0
-    print('valor1: ' + str(((x**2)/(a**2))-((y**2)/(b**2))) + ' , 32/c = ' + str(32/c))
+    print("El valor de altura del punto silla es: ", M)
+    print("\nPrimera condición cumplida...")
+    print('Valor 1: ' + str(((x**2)/(a**2))-((y**2)/(b**2))) + ' , 32/c = ' + str(32/c))
     if ((x**2)/(a**2))-((y**2)/(b**2)) == (32/c):
         print('Valor1 = True')
         valor_1 = True
     else:
          valor_1 = False
-    print('si')
-    print('valor2: ' + str(8 + (c*(y**2))/(4*(b**2))) + ', valor2 = ' + str(((c*(x**2))/(4*(a**2)))))
-    valor_3 = (8 + (c*(y**2)))/(4*(b**2))
+    print("\nSegunda condición cumplida...")
+    print('Valor 2: ' + str(8 + (c*(y**2))/(4*(b**2))) + ', Valor 2 = ' + str(((c*(x**2))/(4*(a**2)))))
+    valor_3 = 8 + (c*(y**2))/(4*(b**2))
+    Hz = valor_3
+    print("Hz es igual a: ", Hz)
     if (8 + (c*(y**2))/(4*(b**2))) == ((c*(x**2))/(4*(a**2))):
         print('Valor2 = True')
-        Hz = valor_3
-        print(Hz)
         valor_2 = True
     else: 
         valor_2 = False
     #if valor_1 and valor_2:
-    print(M)
-    vVault = ((4*a*c)/b**3)*((y/4)*((y**2/4)+(M*b**2/c))**(3/2) +
-                    ((3*M*b**2*y)/(8*c))*sqrt((y**2/4)+(M*b**2/c)) +
-                         ((3*(M**2)*(b**4))/(8*c**2)) * (log((sqrt(((y**2)/4) +
-                             (M*b**2/c))+y/2)/(sqrt(((y**2)/4) + (M*b**2/c))-y/2))))
-    print(vVault)
+
+    M2 = 7.88
+
+    vVault = ((4 * a * c) / b ** 3) * ((y / 4) * ((y ** 2 / 4) + (M * b ** 2 / c)) ** (3 / 2) +
+                    ((3 * M * b ** 2 * y) / (8 * c)) * sqrt((y ** 2 / 4) + (M * b ** 2 / c)) +
+                                 ((3 * (M ** 2) * (b ** 4)) / (8 * c ** 2)) * (log((sqrt(((y ** 2) / 4) +
+                                     (M * b ** 2 / c)) + y / 2) / (sqrt(((y ** 2) / 4) + (M * b ** 2 / c)) - y / 2))))
+
+    vVault2 = ((4 * a * c) / b ** 3) * ((y / 4) * ((y ** 2 / 4) + (M2 * b ** 2 / c)) ** (3 / 2) +
+                    ((3 * M2 * b ** 2 * y) / (8 * c)) * sqrt((y ** 2 / 4) + (M2 * b ** 2 / c)) +
+                                ((3 * (M2 ** 2) * (b ** 4)) / (8 * c ** 2)) * (log((sqrt(((y ** 2) / 4) +
+                                    (M2 * b ** 2 / c)) + y / 2) / (sqrt(((y ** 2) / 4) + (M2 * b ** 2 / c)) - y / 2))))
+
+    print("\nLa cantidad de volumen de la bóveda cuando M es igual a 8 es: ", vVault)
+    print("La cantidad de volumen de la bóveda cuando M es igual a 7.88 es: ", vVault2)
+    print("\nLa cantidad de cemento requerida para la bóveda es de: ", vVault-vVault2, "(m3) metros cúbicos de cemento")
     #else:
     # return(a,b,c,"Falso")
 def main():
-    m = 7.88
+    m = 8
     lx = 16
     ly = 50
     a = 1
     b = 3.5
     c = 0.617
-    print(formulas(a,b,c,lx,ly,m))
+    formulas(a,b,c,lx,ly,m)
 if __name__ == '__main__':
     main()
